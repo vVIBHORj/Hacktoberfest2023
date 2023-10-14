ed9fb52f502ea42478c067eb2cfcdf1ffde40f51
vVIBHORj <137258447+vVIBHORj@users.noreply.github.com>
2023-10-14 11:19:02 +0530

    Update factorial.py
    updated

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,3 +1,17 @@
-import math
-n=int(input("Enter a number: "))
-print("Factorial of",n,"is",math.factorial(n))
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+# Input from the user
+num = int(input("Enter a number:"))
+
+# Calculate and display the factorial
+print(f"Factorial of {num} is {factorial(num)}")
+
